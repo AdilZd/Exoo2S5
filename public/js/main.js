d66e79f1014afed571ca3e317b651ffc461a42ca
AdilZd <nowayat0me@gmail.com>
2022-12-21 14:07:53 +0100

    Finalité

---
+++ public/js/main.js
@@ -0,0 +1,73 @@
+// 1 // 
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 3 === 0) {
+//     continue;
+//     }
+//     console.log(i);
+// }
+
+// 2 //
+
+// let paire = prompt (" Ecrire un nombre paire :");
+// while (true) {
+//     paire = prompt(" Ecrire un nombre paire :");
+//     if (num % 2 === 0) {
+//     break;
+//     }
+// }
+
+
+// 3 //
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+//     if (i === 7) {
+//     break;
+//     }
+// }
+
+
+// 4 //
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 2 == 1) {
+//     continue;
+//     }
+//     console.log(i);
+// }
+
+
+// 5 //
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+//     if (i == 5) {
+//     break;
+//     }
+// }
+// console.log(` Interomppue à 5 `);
+
+
+// 6 // 
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 3 === 0) {
+//     console.log("Itération sautée.");
+//     continue;
+//     }
+//     console.log(i);
+// }
+
+
+// 7 //
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i > 5) {
+//     break;
+//     }
+//     console.log(i);
+// }
+
+// console.log("La boucle a été interrompue.");
+
